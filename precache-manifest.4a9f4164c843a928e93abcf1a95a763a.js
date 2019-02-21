446826c16d808658d1b2ddb8e3b49bd85f6d42fc
Naim Berk TUMER <naimberktumer@gmail.com>
2019-02-22 02:01:30 +0300

    Updates

---
+++ precache-manifest.4a9f4164c843a928e93abcf1a95a763a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89679d71dd217ecf3d77",
+    "url": "/Change-me-plz-zahit/static/css/main.daedcb28.chunk.css"
+  },
+  {
+    "revision": "89679d71dd217ecf3d77",
+    "url": "/Change-me-plz-zahit/static/js/main.89679d71.chunk.js"
+  },
+  {
+    "revision": "a9a70befdbecc38ac78e",
+    "url": "/Change-me-plz-zahit/static/js/runtime~main.a9a70bef.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Change-me-plz-zahit/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Change-me-plz-zahit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e5d15d2c4e54f3ab9747c46b118c7f62",
+    "url": "/Change-me-plz-zahit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c06a9fb40aa151f5712709a3cdad9b0e.js"
+  "/Change-me-plz-zahit/precache-manifest.4a9f4164c843a928e93abcf1a95a763a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Change-me-plz-zahit/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
