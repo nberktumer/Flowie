b59a3a3d4bc8d3b16075d043d7db1ab6b93ad6b2
Naim Berk TUMER <naimberktumer@gmail.com>
2019-05-05 16:57:40 +0300

    Updates

---
+++ precache-manifest.ea03a5561e1093eb209efdb50c7b47b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f149f083d93f4270dcf",
+    "url": "/Flowie/static/css/main.f3050959.chunk.css"
+  },
+  {
+    "revision": "9f149f083d93f4270dcf",
+    "url": "/Flowie/static/js/main.9f149f08.chunk.js"
+  },
+  {
+    "revision": "e9c6ec87bbecff066042",
+    "url": "/Flowie/static/js/runtime~main.e9c6ec87.js"
+  },
+  {
+    "revision": "bac7523fe5ff81db68e5",
+    "url": "/Flowie/static/css/2.2bad0162.chunk.css"
+  },
+  {
+    "revision": "bac7523fe5ff81db68e5",
+    "url": "/Flowie/static/js/2.bac7523f.chunk.js"
+  },
+  {
+    "revision": "759dd9f7b4a8fd6820055fa42667705f",
+    "url": "/Flowie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Flowie/precache-manifest.0989953d39289d223baab7726c54b1e7.js"
+  "/Flowie/precache-manifest.ea03a5561e1093eb209efdb50c7b47b4.js"
 );
 
 workbox.clientsClaim();
