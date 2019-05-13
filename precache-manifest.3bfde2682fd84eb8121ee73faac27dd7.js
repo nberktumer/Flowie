dcacf8912d065c9539530712c969a2cf61164e69
Naim Berk TUMER <naimberktumer@gmail.com>
2019-05-14 00:07:24 +0300

    Updates

---
+++ precache-manifest.3bfde2682fd84eb8121ee73faac27dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d488d442645e4b74b17acdfbcbdcb2",
+    "url": "/Flowie/index.html"
+  },
+  {
+    "revision": "873de1fb819d87fdec33",
+    "url": "/Flowie/static/css/2.93c88d9a.chunk.css"
+  },
+  {
+    "revision": "fc9038566756fb9ddfc2",
+    "url": "/Flowie/static/css/main.719be44c.chunk.css"
+  },
+  {
+    "revision": "873de1fb819d87fdec33",
+    "url": "/Flowie/static/js/2.95afbb0e.chunk.js"
+  },
+  {
+    "revision": "fc9038566756fb9ddfc2",
+    "url": "/Flowie/static/js/main.f4486798.chunk.js"
+  },
+  {
+    "revision": "7c24fb178e8f8ca65eb2",
+    "url": "/Flowie/static/js/runtime~main.e952708d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Flowie/precache-manifest.ea03a5561e1093eb209efdb50c7b47b4.js"
+  "/Flowie/precache-manifest.3bfde2682fd84eb8121ee73faac27dd7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Flowie/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Flowie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
